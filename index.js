66f0549fe612f9248439896d54fc8c5e5beb569b
Ricardo Saracino <rsaracino@pm.me>
2018-11-21 08:06:02 -0500

    added pdfmake

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 let fs = require('fs');
 
-let PdfKit = require('pdfkit');
-
 let SignPdf = require('node-signpdf').SignPdf;
 
 let addSignaturePlaceholder = require('./node_modules/node-signpdf/dist/helpers.js').addSignaturePlaceholder;
 
-
+let PdfPrinter = require('pdfmake');
 
 let signer = new SignPdf();
 
-let pdf = new PdfKit();
+var fonts = {
+    Roboto: {
+        normal: 'fonts/Roboto-Regular.ttf',
+        bold: 'fonts/Roboto-Medium.ttf',
+        italics: 'fonts/Roboto-Italic.ttf',
+        bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+    }
+};
+
+var printer = new PdfPrinter();
+
+var docDefinition = {
+    content: [
+        'First paragraph',
+        'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+    ]
+};
+
+var pdf = printer.createPdfKitDocument(docDefinition);
+
 
 let buffers = [];
 
@@ -27,24 +44,6 @@ pdf.on('end', () => {
     writeStream.write(signedPdf);
     writeStream.end();
 });
-
-
-// Set a title and pass the X and Y coordinates
-pdf.fontSize(25).text('Lorem Ipsum', 50, 50);
-
-// Set the paragraph width and align direction
-pdf.fontSize(15).text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget dolor vitae tortor iaculis cursus ' +
-    'sed hendrerit sapien. Duis vel turpis quis lorem rutrum commodo a nec orci. Integer eget ex fermentum, ' +
-    'finibus ipsum a, convallis nisl. Vivamus sodales, neque ut tristique volutpat, nisi elit congue quam, quis ' +
-    'scelerisque sem velit in justo. Sed at ante ut massa tempor vulputate sed ac diam. Quisque posuere ligula nec ' +
-    'tincidunt tincidunt. Etiam ac accumsan sem, elementum lobortis urna. Phasellus venenatis, lectus eget convallis ' +
-    'condimentum, elit risus elementum nisi, commodo aliquet magna sapien nec eros. Quisque iaculis sit amet arcu eu ' +
-    'tempus. Curabitur lectus justo, tincidunt tincidunt elit vitae, dapibus pulvinar nisi.', {
-    width: 410,
-    align: 'left'
-});
-
-// write buffer instead pdf.pipe(fs.createWriteStream('output.pdf'));
 
 
 // Externally (to PDFKit) add the signature placeholder.
